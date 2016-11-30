4d94d5525a1af08dbe4a81be437e804d7abcf296
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-30 18:21:17 +0530

    added basic gpio functions

--- AVR_GPIO.c
+++ AVR_GPIO.c
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////
-// AVR GPIOLIBRARY
+// AVR GPIO LIBRARY
 //
 // NOVEMBER 30, 2016
 //
@@ -8,3 +8,44 @@
 //////////////////////////////////////////////
 
 #include "AVR_GPIO.h"
+
+void AVR_GPIO_pin_set_direction(uint8_t pin, uint8_t direction_register, uint8_t dir)
+{
+	//SET THE DIRECTION OF THE SPECIFIED PIN WITH THE SPECIFIED DIRECTION
+	//REGISTER
+
+	if(dir == AVR_GPIO_PIN_DIR_OUTPUT)
+	{
+		//OUTPUT
+		direction_register |= (1 << pin);
+	}
+	else
+	{
+		//INPUT
+		direction_register &= ~(1 << pin);
+	}
+}
+
+void AVR_GPIO_pin_set_value(uint8_t pin, uint8_t value_register, uint8_t val)
+{
+	//SET THE VALUE OF THE PIN IN IT'S VALUE REGISTER
+	//THIS MAY HAVE DIFFERENT EFFECTS DEPENDING ON IF THE PIN IS CONFIGURED
+	//AS AN INPUT OR OUTPUT
+
+	if(val == AVR_GPIO_PIN_VAL_HIGH)
+	{
+		value_register |= (1 << pin);
+	}
+	else
+	{
+		value_register &= ~(1 << pin);
+	}
+}
+
+void AVR_GPIO_pin_toggle_value(uint8_t pin, uint8_t pin_register)
+{
+	//TOGGLE THE VALUE OF THE SPECIFIED PINS OUTPUT (IN THE PORT REGISTER)
+	//FINAL EFFECT DEPENDS ON WEATHER THE PIN IS SPECIFIED AS INPUT OR OUTPUT
+
+	pin_register |= (1 << pin);
+}
--- AVR_GPIO.h
+++ AVR_GPIO.h
@@ -10,5 +10,29 @@
 #ifndef _AVR_GPIO_H_
 #define _AVR_GPIO_H_
 
+#include <stdio.h>
+#include <avr/io.h>
+
+#define AVR_GPIO_PIN_DIR_INPUT 	0
+#define AVR_GPIO_PIN_DIR_OUTPUT 1
+
+#define AVR_GPIO_PIN_VAL_HIGH 	1
+#define AVR_GPIO_PIN_VAL_LOW 	0
+
+#define AVR_GPIO_PORTB_DIR_REGISTER DDRB
+#define AVR_GPIO_PORTC_DIR_REGISTER DDRC
+#define AVR_GPIO_PORTD_DIR_REGISTER DDRD
+
+#define AVR_GPIO_PORTB_VAL_REGISTER PORTB
+#define AVR_GPIO_PORTC_VAL_REGISTER PORTC
+#define AVR_GPIO_PORTD_VAL_REGISTER PORTD
+
+#define AVR_GPIO_PORTB_PIN_REGISTER PINB
+#define AVR_GPIO_PORTC_PIN_REGISTER PINC
+#define AVR_GPIO_PORTD_PIN_REGISTER PIND
+
+void AVR_GPIO_pin_set_direction(uint8_t pin, uint8_t direction_register, uint8_t dir);
+void AVR_GPIO_pin_set_value(uint8_t pin, uint8_t value_register, uint8_t val);
+void AVR_GPIO_pin_toggle_value(uint8_t pin, uint8_t pin_register);
 
 #endif
